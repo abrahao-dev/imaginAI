8767ccbce1f55e78593b7b6ca87e655d09247a17
abrahao-dev <matheus.alvares23@outlook.com>
2024-09-17 14:32:49 -0300

    Initial Commit

---
+++ ImaginAI/ImaginAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImaginAIApp.swift
+//  ImaginAI
+//
+//  Created by Matheus Abrahao on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ImaginAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
